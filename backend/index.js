ae7b134d868152b030db681054d7a493266eaf4f
Rajdev Kumar <rajdev@crowdkeep.com>
2023-10-17 16:50:42 +0100

    Variaveis de Ambiente

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
+require('dotenv').config()
 
 app.get('/', (req, res) => {
-  res.send('Backend')
+  res.send("BACKEND")
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
 })
