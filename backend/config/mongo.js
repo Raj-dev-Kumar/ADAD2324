06b43a19c6773c1c202315053e2363208456ea1a
Rajdev Kumar <rajdev@crowdkeep.com>
2023-10-18 11:11:26 +0100

    Conexão com MongoDB

---
+++ backend/config/mongo.js
@@ -0,0 +1,30 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+
+// Connection URL
+const url = process.env.MONGOURL;
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'project';
+
+
+  // Use connect method to connect to the server
+  try {
+    client.connect();
+    console.log('Conexão com servidor MongoDB realizada com sucesso');
+    const db = client.db(dbName);
+    module.exports = db
+    
+  } catch (error) {
+    console.log("Error")
+    
+  }
+
+
+  // the following code examples can be pasted here...
+
+
+
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const mongo = require("./../config/mongo.js")
 
 // middleware that is specific to this router
 router.use((req, res, next) => {
@@ -7,7 +8,9 @@ router.use((req, res, next) => {
 })
 // define the home page route
 router.get('/', (req, res) => {
-  res.send('Show All Users')
+  //res.send(`Show All Users`)
+  res.send(mongo.collection("users").find().toArray())
+  
 })
 
 module.exports = router
