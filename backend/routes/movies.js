a21953ece568cf1a68a69fe9de8e7105d00fc679
Rajdev Kumar <rajdev@crowdkeep.com>
2023-10-18 11:52:51 +0100

    implementação do Getmovie e GetmovieByID

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -1,13 +1,27 @@
 const express = require('express')
 const router = express.Router()
+const mongo = require("./../config/mongo.js")
 
 // middleware that is specific to this router
 router.use((req, res, next) => {
     next()
 })
-// define the home page route
-router.get('/', (req, res) => {
-  res.send('Show All Movies')
+
+// Utilizador por ID
+router.get('/:movieid', async (req, res) => {
+
+  var id_aprocurar = parseInt(req.params.movieid)
+  
+  movies = await mongo.collection("movies").find({"_id":id_aprocurar}).toArray()
+  res.send(movies)
+  
+})
+
+// Todos os utilizadores - (Limitado a 50)
+router.get('/', async (req, res) => {
+  movies = await mongo.collection("movies").find({}).limit(50).toArray()
+  res.send(movies)
+  
 })
 
 module.exports = router
