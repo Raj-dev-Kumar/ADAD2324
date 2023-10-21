4850fdde0f3772cd6263d05b7612e34a696d54e7
Rajdev Kumar <rajdev@crowdkeep.com>
2023-10-21 21:58:01 +0100

    Implementação do Crud em /api/movies

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const mongo = require("./../config/mongo.js")
+const tipoObjectId = require("mongodb").ObjectId
+
+
+const moviesCollection = mongo.collection("movies")
 
 // middleware that is specific to this router
 router.use((req, res, next) => {
@@ -12,14 +16,36 @@ router.get('/:movieid', async (req, res) => {
 
   var id_aprocurar = parseInt(req.params.movieid)
   
-  movies = await mongo.collection("movies").find({"_id":id_aprocurar}).toArray()
+  movies = await moviesCollection.find({"_id":id_aprocurar}).toArray()
+  if(movie.length<1)
+  movies = await moviesCollection.find({"_id":new tipoObjectId(req.params.movieid)}).toArray()
+
   res.send(movies)
+  
+})
+
+router.delete('/:movieid', async (req, res) => {
+
+  var id_aprocurar = parseInt(req.params.movieid)
+
+  var EliminarMovie = await moviesCollection.deleteMany({"_id":id_aprocurar})
+  if (EliminarMovie.deletedCount <1 )
+  EliminarUtilizador = await moviesCollection.deleteMany({"_id":new tipoObjectId(req.params.movieid)})
+  
+  res.json({"Resultado":EliminarUtilizador})
+  
+})
+
+router.post('/', async (req, res) => {
+
+  var adicionarMovies = await moviesCollection.insertMany(req.body.movies)
+  res.json({"Resultado":adicionarMovies})
   
 })
 
 // Todos os utilizadores - (Limitado a 50)
 router.get('/', async (req, res) => {
-  movies = await mongo.collection("movies").find({}).limit(50).toArray()
+  movies = await moviesCollection.find({}).limit(50).toArray()
   res.send(movies)
   
 })
