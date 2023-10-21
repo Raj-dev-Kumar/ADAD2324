2c82412f903dd593c201dd1ed43136b74ecc0365
Rajdev Kumar <rajdev@crowdkeep.com>
2023-10-21 18:01:44 +0100

    Eliminar Utilizadores

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,11 @@
+// Imports
 const express = require('express')
 const router = express.Router()
 const mongo = require("./../config/mongo.js")
+const tipoObjectId = require("mongodb").ObjectId
 
+//Variaveis Globais
+const usercollection = mongo.collection("users")
 
 router.use((req, res, next) => {
     next()
@@ -11,26 +15,37 @@ router.use((req, res, next) => {
 router.get('/:userid', async (req, res) => {
 
   var id_aprocurar = parseInt(req.params.userid)
-  var usercollection = mongo.collection("users")
   
   users = await usercollection.find({"_id":id_aprocurar}).toArray()
+  if(users.length<1)
+  users = await usercollection.find({"_id":new tipoObjectId(req.params.userid)}).toArray()
 
   res.send(users)
   
 })
 
+router.delete('/:userid', async (req, res) => {
+
+  var id_aprocurar = parseInt(req.params.userid)
+
+  var EliminarUtilizador = await usercollection.deleteMany({"_id":id_aprocurar})
+  if (EliminarUtilizador.deletedCount <1 )
+  EliminarUtilizador = await usercollection.deleteMany({"_id":new tipoObjectId(req.params.userid)})
+  
+  res.json({"Resultado":EliminarUtilizador})
+  
+})
+
 router.post('/', async (req, res) => {
 
-  var usercollection = mongo.collection("users")
   var adicionarUtilizador = await usercollection.insertMany(req.body.users)
-  res.json({"Resultado":req.body.users,"MN":adicionarUtilizador})
+  res.json({"Resultado":adicionarUtilizador})
   
 })
 
 // Todos os utilizadores - (Limitado a 50)
 router.get('/', async (req, res) => {
   
-  var usercollection = mongo.collection("users")
   users = await usercollection.find({}).limit(50).toArray()
   res.send(users)
   
